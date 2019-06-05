60f29efe258c6dc05299fe80848e39fe2ea0e317
Aurélia <aurelia.h.vacher@gmail.com>
2019-06-05 13:53:04 +0300

    build

---
+++ precache-manifest.9f48638f578685486d532303e792b665.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe21ba8014ef52ef4c4ba964029a2907",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd5443a6c10768a796bc",
+    "url": "/static/css/main.3d8e9d32.chunk.css"
+  },
+  {
+    "revision": "49fe2bb6337391b45054",
+    "url": "/static/js/2.6e39e941.chunk.js"
+  },
+  {
+    "revision": "dd5443a6c10768a796bc",
+    "url": "/static/js/main.07853e53.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "44a7ed589f7586ac06b207919a3ee043",
+    "url": "/static/media/Linkedin_circle.svg_.44a7ed58.png"
+  },
+  {
+    "revision": "96e038b16eefb10ac1300a893f9793fb",
+    "url": "/static/media/georgie-cobbs-459520-unsplash.96e038b1.jpg"
+  }
+]);
